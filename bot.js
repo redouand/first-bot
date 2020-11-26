00d69184225859028c1510def15c236318082ce2
Your Name <theomurf47@gmail.com>
2020-11-26 17:13:05 +0100

    add sandbox

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer')
 
 async function bot() {
     try {
-        const browser = await puppeteer.launch({ headless: true })
+        const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] })
         const page = await browser.newPage()
         await page.goto('https://reds-shnack.herokuapp.com/', { waitUntil: 'load', timeout: 0 })
 
